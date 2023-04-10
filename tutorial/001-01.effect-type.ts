cf4609faed82195fd4f10903194035d212f56a58
lee gukbeom <tmwlak@gmail.com>
2023-04-10 17:55:50 +0900

    1

--- tutorial/001-01.effect-type.ts
+++ tutorial/001-01.effect-type.ts
@@ -4,6 +4,11 @@ import * as Effect from "@effect/io/Effect";
 import * as Layer from "@effect/io/Layer";
 import * as Context from "@effect/data/Context";
 
+// 배운 점
+// effect 데이터 타입은 primitive data type으로 만들 수도 있고 함수로 만들 수도 있다
+// 의문점 : effect 데이터 타입을 왜 굳이 primitive data type으로 만드는지는 의문이다
+// 그냥 primitive data type를 바로 사용하면 되는 것이 아닌가
+
 export const succeed = Effect.succeed(7);
 
 export const fail = Effect.fail(3);
--- tutorial/001-10.contro_flow v1.ts
+++ tutorial/001-10.contro_flow v1.ts
@@ -12,32 +12,30 @@ import { log } from "console";
  * The following is an example of a computation that can fail. We will look at
  * more error handling in a later chapter.
  */
+
+const createRandom = () => Math.random()
+
 function isAbove0_5(random: number): Either.Either<"fail", number> {
   return random > 0.5 
         ? Either.right(random) 
         : Either.left("fail" as const);
 }
 
-const createRandom = () => Math.random()
 // This will fail sometimes
 export const flakyEffect = pipe(
   createRandom,
-  Effect.sync, // Effect.Effect<never, never, number>
+  Effect.sync, // effect 데이터를 생성한다. Effect.Effect<never, never, number>
   Effect.map(isAbove0_5), // Effect.Effect<never, never, Either<'fail', number>>
   Effect.flatMap(Effect.fromEither), // Effect.Effect<never, 'fail', number>
-  // Effect.reduceAll
+  // 결국 pipe의 최종 리턴값은 Effect 데이터가 될 것이다 
+  // 그리고 이 effect는 바로 실행되지 않는다
+  // effect 데이터는 run을 통해서만 실행된다. 이러한 게으름(laziness)는 effect의 큰 특징이다
+  // 이렇게 함으로서 제어권을 가지게 된다
 );
 
-
-
-
-// flakyEffect : EffectPrimitive
 const res = Effect.runSync(flakyEffect)
 console.log(res);
 
-// log( flakyEffect)
-
-// Effect.run
 
 // Same thing but using the number generator provided by Effect
 // export const flakyEffectAbsolved = pipe(
